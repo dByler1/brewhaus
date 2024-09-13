4e835db5b97155d52c5348125b5a914f22a2c78b
Dan Byler <dannybyler@gmail.com>
2024-09-13 11:01:14 -0400

    base url update

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  base: 'https://dbyler1.github.io/brewhaus/',
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
