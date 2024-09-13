18611b429ae8085e7aac61ac4879d36289130b4c
Dan Byler <dannybyler@gmail.com>
2024-09-13 11:12:06 -0400

    and another one

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  base: './',
+  base: '/brewhaus/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
